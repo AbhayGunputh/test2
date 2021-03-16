5205beb810b32b385146066c4a1cc99925b8b629
AbhayGunputh <kumarabhay150499@gmail.com>
2021-03-16 13:18:12 +0400

    [Main] First modification

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # test2
+This python program will output the first and last numbers from the array.
---
+++ python-Test.py
@@ -0,0 +1,5 @@
+#!/bin/bash/env python3
+
+Z =[12, 34, 45, 34, 78, 19, 500]
+print(Z[0])
+print(Z[-1])
